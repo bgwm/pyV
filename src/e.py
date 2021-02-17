c367f5fdb473d50fb17d0dc550768815f1f14ef6
BgWm <Bigwormsben@gmail.com>
2021-02-18 02:08:48 +0800

    x

--- src/e.py
+++ src/e.py
@@ -13,7 +13,7 @@ VIVO_C = ["#415fff"
 
 f, ax = plt.subplots()
 sns.set_theme()
-sns.axes_style("darkgrid")
+sns.set_style("darkgrid", {'figure.facecolor':'blue'})
 
 tips = pd.read_csv('~/Project/pyV/dataset/tips.csv')
 
@@ -21,15 +21,16 @@ sns.set_palette(VIVO_C)
 
 
 #ax.legend(ncol=2, loc='lower right', frameon=False)
-ax.patch.set_facecolor(VIVO_C[5])
+ax.patch.set_facecolor('white')#VIVO_C[5])
 ax.grid(True, axis='y', which='major',color='black',
     alpha=0.1)
+ax.set_axisbelow(True)
 #ax.set_xticks([])
 #ax.axes.get_yaxis().set_visible(True)
 
 g = sns.histplot(data=tips, x="total_bill", hue="sex", 
     bins=35, multiple="stack",
-    linewidth=0, shrink=.8)
+    linewidth=0, shrink=.8, alpha=1)
 
 sns.despine(right=True
     , left=True
@@ -37,8 +38,36 @@ sns.despine(right=True
     #, bottom=True
     )
 
-leg = g.legend_ #leg = g.axes.flat[0].get_legend()
-leg.set_title('check')
+# --------
+#leg = g.legend_ #leg = g.axes.flat[0].get_legend()
+
+ax.legend(labels=('First', 'Second')
+    , loc='upper right', bbox_to_anchor=(1, 1.07)
+    , fontsize='x-small'
+    , frameon=False
+    , ncol=2)
+
 plt.show()
-f.savefig("out.png")
+f.savefig("e.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
